576ada30dfbd63725c098cffa108968f3dbecf1b
kalebbroo <kalebbroo@gmail.com>
2023-06-18 16:38:33 -0400

    fixed admin commands

--- core/admin_tools.py
+++ core/admin_tools.py
@@ -2,6 +2,7 @@ from mee6_py_api import API
 from discord.ext import commands
 from discord import app_commands, Member, Embed, Colour
 from discord.utils import get
+import discord
 #from discord.app_commands import OptionType
 
 
@@ -9,7 +10,7 @@ class AdminTools(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name='meesux', description='Update XP and level from Mee6 leaderboard')
+    @app_commands.command(name='meesux', description='Import XP and level from Mee6 leaderboard')
     @app_commands.checks.has_permissions(administrator=True)
     async def meesux(self, interaction):
         try:
@@ -21,21 +22,23 @@ class AdminTools(commands.Cog):
                 user["xp"] = player.xp
                 user["level"] = player.level
                 self.update_user(user)
-            await interaction.channel.send("XP and level data has been updated from the Mee6 leaderboard.")
+            await interaction.channel.send("XP and level data has been imported from the Mee6 leaderboard.")
         except Exception as e:
             await interaction.channel.send(f"An error occurred: {e}")
 
 
     @app_commands.command(name='mute', description='Mute a member')
+    @app_commands.describe(Member='The member to mute')
+    @app_commands.describe(reason='The reason for the mute')
     @app_commands.checks.has_permissions(administrator=True)
-    async def mute(self, interaction, reason: str = None):
+    async def mute(self, interaction, member: discord.Member, reason: str = None):
         try:
             role = get(interaction.guild.roles, name="Muted")
             if not role:
                 role = await interaction.guild.create_role(name="Muted")
                 for channel in interaction.guild.channels:
                     await channel.set_permissions(role, speak=False, send_messages=False)
-            member = member
+            member = discord.Member
             await member.add_roles(role, reason=reason)
             embed = Embed(title="Mute", description=f"{member.mention} has been muted.", color=Colour.red())
             if reason:
@@ -45,46 +48,51 @@ class AdminTools(commands.Cog):
             await interaction.channel.send(f"An error occurred: {e}")
 
     @app_commands.command(name='unmute', description='Unmute a member')
+    @app_commands.describe(user='The member to mute')
+    @app_commands.describe(reason='The reason for the mute')
     @app_commands.checks.has_permissions(administrator=True)
-    async def unmute(self, interaction):
-        member = member
+    async def unmute(self, interaction, user: discord.Member):
         try:
             role = get(interaction.guild.roles, name="Muted")
-            if role in member.roles:
-                await member.remove_roles(role)
-                embed = Embed(title="Unmute", description=f"{member.mention} has been unmuted.", color=Colour.green())
+            if role in user.roles:
+                await user.remove_roles(role)
+                embed = Embed(title="Unmute", description=f"{user.mention} has been unmuted.", color=Colour.green())
                 await interaction.channel.send(embed=embed)
             else:
-                await interaction.channel.send(f"{member.mention} is not muted.")
+                await interaction.channel.send(f"{user.mention} is not muted.")
         except Exception as e:
             await interaction.channel.send(f"An error occurred: {e}")
 
     @app_commands.command(name='kick', description='Kick a member from the server')
+    @app_commands.describe(user='The member to kick')
+    @app_commands.describe(reason='The reason for the kick')
     @app_commands.checks.has_permissions(administrator=True)
-    async def kick(self, interaction, reason: str = None):
-        member = member
-        await member.kick(reason=reason)
-        await interaction.channel.send(f"{member.mention} has been kicked for {reason}.")
+    async def kick(self, interaction, user: discord.Member, reason: str = None):
+        await user.kick(reason=reason)
+        await interaction.channel.send(f"{user.mention} has been kicked for {reason}.")
 
     @app_commands.command(name='ban', description='Ban a member from the server')
+    @app_commands.describe(user='The member to ban')
+    @app_commands.describe(reason='The reason for the ban')
     @app_commands.checks.has_permissions(administrator=True)
-    async def ban(self, interaction, reason: str = None):
-        member = member
-        await member.ban(reason=reason)
-        await interaction.channel.send(f"{member.mention} has been banned for {reason}.")
+    async def ban(self, interaction, user: discord.Member, reason: str = None):
+        await user.ban(reason=reason)
+        await interaction.channel.send(f"{user.mention} has been banned for {reason}.")
 
     @app_commands.command(name='change_role', description='Change a user\'s role')
+    @app_commands.describe(user='The user to change the role of')
+    @app_commands.describe(role='The role to give the user')
     @app_commands.checks.has_permissions(administrator=True)
-    async def change_role(self, interaction, role: str = None):
-        member = member
+    async def change_role(self, interaction, user: discord.Member, role: str = None):
         try:
-            await member.add_roles(role)
-            await interaction.channel.send(f"{member.mention} has been given the {role.name} role.")
+            await user.add_roles(role)
+            await interaction.channel.send(f"{user.mention} has been given the {role.name} role.")
         except Exception as e:
             await interaction.channel.send(f"An error occurred: {e}")
 
 
-    @app_commands.command(name='send_message', description='Send a message to all members')
+    @app_commands.command(name='send_message', description='Send a message to all users')
+    @app_commands.describe(message='The message to send to all users')
     @app_commands.checks.has_permissions(administrator=True)
     async def send_message(self, interaction, *, message: str = None):
         for member in interaction.guild.members:
@@ -101,12 +109,12 @@ class AdminTools(commands.Cog):
         await interaction.channel.send(embed=embed)
 
     @app_commands.command(name='user_stats', description='Display user stats')
+    @app_commands.describe(user='The member to mute')
     @app_commands.checks.has_permissions(administrator=True)
-    async def user_stats(self, interaction):
-        member = member
-        embed = Embed(title=f"{member.name} Stats")
-        embed.add_field(name="Joined At", value=member.joined_at)
-        embed.add_field(name="Roles", value=len(member.roles))
+    async def user_stats(self, interaction, user: discord.Member):
+        embed = Embed(title=f"{user.name} Stats")
+        embed.add_field(name="Joined At", value=user.joined_at)
+        embed.add_field(name="Roles", value=len(user.roles))
         await interaction.channel.send(embed=embed)
 
 async def setup(bot):
