03aceec0b9d0326663182427be909d51c1e6977c
kalebbroo <kalebbroo@gmail.com>
2023-06-19 23:29:51 -0400

    added await

--- core/xp.py
+++ core/xp.py
@@ -27,13 +27,13 @@ class XPCore(commands.Cog):
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
         if before.roles != after.roles:
-            user = self.get_user(after.id)
+            user = await self.get_user(after.id)
             user["roles"] = [role.id for role in after.roles]
             self.update_user(user)
 
 
     async def add_xp(self, user_id, xp):
-        user = self.get_user(user_id)
+        user = await self.get_user(user_id)
         user["xp"] += xp
         level = 1
         while user["xp"] >= ((1.2 ** level - 1) * 100) / 0.2:
@@ -41,13 +41,13 @@ class XPCore(commands.Cog):
         if level > user["level"]:
             user["level"] = level
             print(f"User {user_id} has leveled up to level {level}!")
-        self.update_user(user)  # Update the database with the new XP
+        await self.update_user(user)  # Update the database with the new XP
 
 
     @commands.Cog.listener()
     async def on_thread_create(self, user_id, thread):
         xp = 50
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, user_id, reaction):
@@ -55,70 +55,70 @@ class XPCore(commands.Cog):
         xp = 5
         if reaction.count > 5:
             xp += 5  # Bonus for popular reactions
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_server_boost(self, user_id):
         xp = 500
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_slash_command(self, user_id, command):
         xp = 20
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_invite(self, user_id, invite):
         xp = 200
         if invite.status == "accepted":
             xp += 50  # Bonus for successful invites
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_new_user_engage(self, user_id, message):
         xp = 30
         if message.reply_count > 5:
             xp += 10  # Bonus for engaging with new users
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_stream_start(self, user_id, stream):
         xp = 3 * stream.duration
         if stream.viewer_count > 2:
             xp += stream.viewer_count  # Bonus for popular streams
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_event_participate(self, user_id, event):
         xp = 100
         if event.participant_count > 5:
             xp += 20  # Bonus for popular events
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_long_message(self, user_id, message):
         xp = 20
         if len(message) > 200:
             xp += 1 * (len(message) - 200) // 10  # Bonus for long messages
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_message_pinned(self, user_id, message):
         xp = 1000
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     @commands.Cog.listener()
     async def on_voice_chat_participation(self, user_id, duration):
         xp = 2 * duration  # Example: 2 XP per minute of voice chat
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     async def on_daily_login(self, user_id):
         xp = 50  # Example: 50 XP for daily login
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
     async def on_weekly_challenge_completion(self, user_id):
         xp = 500  # Example: 500 XP for completing a weekly challenge
-        self.add_xp(user_id, xp)
+        await self.add_xp(user_id, xp)
 
 
 
@@ -139,8 +139,8 @@ class XPCore(commands.Cog):
             user["spam_count"] = 0
         user["last_message_time"] = time.time()
         user["message_count"] += 1  # Increment message count
-        self.update_user(user)
-        self.add_xp(user_id, xp)
+        await self.update_user(user)
+        await self.add_xp(user_id, xp)
 
 async def setup(bot:commands.Bot):
     await bot.add_cog(XPCore(bot))
